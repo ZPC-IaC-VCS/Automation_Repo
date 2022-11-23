b7e59d0d68cff676830ce273d226a7e2c19b5428
ZPC-IaC-VCS <100279545+ZPC-IaC-VCS@users.noreply.github.com>
2022-11-23 17:09:36 +0530

    Update remote.tf

--- remote.tf
+++ remote.tf
@@ -2,10 +2,11 @@ module "vpc_example_complete-vpc" {
   source  = "terraform-aws-modules/vpc/aws//examples/complete-vpc"
   version = "3.14.4"
 }
+  
  module "iam_example_iam-account" {
   source  = "terraform-aws-modules/iam/aws//examples/iam-account"
   version = "5.4.0"
-}. Working fine
+}
 
 
 module "security-group_example_complete" {
