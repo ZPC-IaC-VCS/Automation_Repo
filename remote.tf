e7da169e32d0fd8fd530aed520f0012a33e62f6f
ZPC-IaC-VCS <100279545+ZPC-IaC-VCS@users.noreply.github.com>
2022-11-23 15:00:52 +0530

    Create remote.tf

---
+++ remote.tf
@@ -0,0 +1,23 @@
+module "vpc_example_complete-vpc" {
+  source  = "terraform-aws-modules/vpc/aws//examples/complete-vpc"
+  version = "3.14.4"
+}
+ module "iam_example_iam-account" {
+  source  = "terraform-aws-modules/iam/aws//examples/iam-account"
+  version = "5.4.0"
+}. Working fine
+
+
+module "security-group_example_complete" {
+  source  = "terraform-aws-modules/security-group/aws//examples/complete"
+  version = "4.13.0"
+} 
+resource "aws_instance" "app_server" {
+  #zpc-skip-policy:ZS-AWS-00029:Please add a comment
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  ingress_cidr_blocks = ["0.0.0.0/16"]
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}  
