7a9d5495b665fee5d070053a5e60edd7aae37ee5
ZPC-IaC-VCS <100279545+ZPC-IaC-VCS@users.noreply.github.com>
2022-11-24 09:48:42 +0530

    Update ec2single.tf

--- ec2single.tf
+++ ec2single.tf
@@ -10,7 +10,7 @@ provider "aws" {
   profile = "default"
   region  = "us-west-2"
 }
-resource "aws_instance" "app_server" {
+resource "aws_instance" "app_server1" {
   #zpc-skip-policy:ZS-AWS-00029:Please add a comment
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
